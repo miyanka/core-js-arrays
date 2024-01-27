509098af92e0c7f1e7746d1906418598fb3fddf8
Yana <bojcenko.jana@gmail.com>
2024-01-27 23:25:07 +0100

    feat: implement the function getIndicesOfOddNumbers

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -430,8 +430,13 @@ function getIdentityMatrix(n) {
  *    getIndicesOfOddNumbers([2, 4, 6, 8, 10]) => []
  *    getIndicesOfOddNumbers([11, 22, 33, 44, 55]) => [0, 2, 4]
  */
-function getIndicesOfOddNumbers(/* numbers */) {
-  throw new Error('Not implemented');
+function getIndicesOfOddNumbers(numbers) {
+  return numbers.reduce((arr, i, ind) => {
+    if (i % 2) {
+      arr.push(ind);
+    }
+    return arr;
+  }, []);
 }
 
 /**
